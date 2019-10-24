8959ef4bdbe26c95fed2da526c41ee899d78c54d
JKY <jky@JKYui-MacBookPro.local>
2019-10-24 22:59:29 +0900

    a

---
+++ clock.js
@@ -0,0 +1,14 @@
+const clockContainer = document.querySelector(".js-clock"),
+clockTitle = clockContainer.querySelector("h1");
+
+
+function getTime(){
+const da
+clockTitle.innerText = "asdfasdfadsf"
+}
+
+function init(){
+getTime()
+};
+addEventListene
+init();
